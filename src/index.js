057b0ec8a1313223d98961370a0e5a5fdc27bb51
mudza <alifulmudzakir@gmail.com>
2019-01-21 14:39:52 +0700

    register auth view

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './component/App';
 import * as serviceWorker from './serviceWorker';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './component/Auth/Login';
 import Register from './component/Auth/Register';
+
+import 'semantic-ui-css/semantic.min.css';
 
 const Root = () => {
   return (
